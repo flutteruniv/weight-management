ada46d769a2c855fc9885aa51e7ec3ad5ac76ed2
wakanao <rockwakata15@bpe.agr.hokudai.ac.jp>
2020-10-14 21:39:42 +0900

    トップページ作りました

--- lib/main.dart
+++ lib/main.dart
@@ -4,63 +4,73 @@ void main() {
   runApp(MyApp());
 }
 
-//aaaaaaaa
 class MyApp extends StatelessWidget {
+  final List<String> _tabNames = [
+    "保存",
+    "比較",
+    "グラフ",
+    "一覧",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(_tabNames[1]),
+        ),
+        body: _topPageBody(context),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.cake),
+              title: Text(
+                _tabNames[0],
+                style: TextStyle(color: Colors.blueGrey),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.grade),
+              title: Text(
+                _tabNames[1],
+                style: TextStyle(color: Colors.blueGrey),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.edit),
+              title: Text(_tabNames[2]),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.pages),
+              title: Text(_tabNames[3]),
             ),
           ],
         ),
+        // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _topPageBody(BuildContext context) {
+    /*final model = Provider.of<TopModel>(context);
+    final currentIndex = model.currentIndex;
+    return Stack(
+      children: <Widget>[
+        _tabPage(currentIndex, 0, HomePage()),
+        _tabPage(currentIndex, 1, VimeoPage()),
+        _tabPage(currentIndex, 2, NotificationPage()),
+        _tabPage(currentIndex, 3, MyPage()),
+      ],
+    );*/
+  }
+
+  Widget _tabPage(int currentIndex, int tabIndex, StatelessWidget page) {
+    return Visibility(
+      visible: currentIndex == tabIndex,
+      maintainState: true,
+      child: page,
     );
   }
 }
---
+++ lib/main_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class TopModel extends ChangeNotifier {
+  Future init() async {
+    notifyListeners();
+  }
+
+  int currentIndex = 0;
+
+  void onTabTapped(int index) {
+    currentIndex = index;
+  }
+}
