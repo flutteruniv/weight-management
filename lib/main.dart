90335411579836a2aeee670f3a63023703600d31
wakanao <rockwakata15@bpe.agr.hokudai.ac.jp>
2020-10-15 14:15:40 +0900

    conflict解消練習

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-    );
+    return MaterialApp();
   }
 }
