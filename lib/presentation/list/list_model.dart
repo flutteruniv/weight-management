4fa5f260bcbe89afb523485bf47a13f3a61abc19
wakanao <rockwakata15@bpe.agr.hokudai.ac.jp>
2020-11-08 17:25:01 +0900

    一覧画面を昇順降順、体重順に変えられるようにした

--- lib/presentation/list/list_model.dart
+++ lib/presentation/list/list_model.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weight_management/domain/muscle_data.dart';
 
 class ListModel extends ChangeNotifier {
   List<MuscleData> muscleData = [];
+  String sortName = '日付順（昇順）';
 
   Future fetchData() async {
     final docs = await FirebaseFirestore.instance
@@ -12,6 +14,97 @@ class ListModel extends ChangeNotifier {
         .get();
     final muscleData = docs.docs.map((doc) => MuscleData(doc)).toList();
     this.muscleData = muscleData;
+    notifyListeners();
+  }
+
+  Future<int> showCupertinoBottomBar(BuildContext context) {
+    //選択するためのボトムシートを表示
+    return showCupertinoModalPopup<int>(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoActionSheet(
+            message: Text('表示順を選ぶ'),
+            actions: <Widget>[
+              CupertinoActionSheetAction(
+                child: Text(
+                  '日付（昇順）',
+                ),
+                onPressed: () {
+                  Navigator.pop(context, 0);
+                },
+              ),
+              CupertinoActionSheetAction(
+                child: Text(
+                  '日付（降順）',
+                ),
+                onPressed: () {
+                  Navigator.pop(context, 1);
+                },
+              ),
+              CupertinoActionSheetAction(
+                child: Text(
+                  '体重順（昇順）',
+                ),
+                onPressed: () {
+                  Navigator.pop(context, 2);
+                },
+              ),
+              CupertinoActionSheetAction(
+                child: Text(
+                  '体重順（降順）',
+                ),
+                onPressed: () {
+                  Navigator.pop(context, 3);
+                },
+              ),
+            ],
+            cancelButton: CupertinoActionSheetAction(
+              child: const Text('キャンセル'),
+              onPressed: () {
+                Navigator.pop(context, 4);
+              },
+              isDefaultAction: true,
+            ),
+          );
+        });
+  }
+
+  void showBottomSheet(BuildContext context) async {
+    //ボトムシートから受け取った値によって操作を変える
+    final result = await showCupertinoBottomBar(context);
+    if (result == 0) {
+      sortName = '日付順（昇順）';
+      final docs = await FirebaseFirestore.instance
+          .collection('muscleData')
+          .orderBy('date', descending: true)
+          .get();
+      final muscleData = docs.docs.map((doc) => MuscleData(doc)).toList();
+      this.muscleData = muscleData;
+    } else if (result == 1) {
+      sortName = '日付順（降順）';
+      final docs = await FirebaseFirestore.instance
+          .collection('muscleData')
+          .orderBy('date', descending: false)
+          .get();
+      final muscleData = docs.docs.map((doc) => MuscleData(doc)).toList();
+      this.muscleData = muscleData;
+    } else if (result == 2) {
+      sortName = '体重順（昇順）';
+      final docs = await FirebaseFirestore.instance
+          .collection('muscleData')
+          .orderBy('weight', descending: true)
+          .get();
+      final muscleData = docs.docs.map((doc) => MuscleData(doc)).toList();
+      this.muscleData = muscleData;
+    } else if (result == 3) {
+      sortName = '体重順（降順）';
+      final docs = await FirebaseFirestore.instance
+          .collection('muscleData')
+          .orderBy('weight', descending: false)
+          .get();
+      final muscleData = docs.docs.map((doc) => MuscleData(doc)).toList();
+      this.muscleData = muscleData;
+    }
     notifyListeners();
   }
 }
--- lib/presentation/list/list_page.dart
+++ lib/presentation/list/list_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weight_management/presentation/list/list_model.dart';
@@ -6,34 +8,42 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<ListModel>(
-      create: (_) => ListModel()..fetchData(),
-      child: Scaffold(
-        body: Consumer<ListModel>(
-          builder: (context, model, child) {
-            final muscleData = model.muscleData;
-            final listTiles = muscleData
-                .map((muscleData) => Container(
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(color: Colors.black38, width: 1),
+        create: (_) => ListModel()..fetchData(),
+        child: Scaffold(
+          body: Consumer<ListModel>(
+            builder: (context, model, child) {
+              final muscleData = model.muscleData;
+              final listTiles = muscleData
+                  .map((muscleData) => Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(color: Colors.black38, width: 1),
+                          ),
                         ),
-                      ),
-                      child: ListTile(
-                          leading: Image.network(muscleData.imageURL),
-                          title: Text(muscleData.weight.toString() + ' kg'),
-                          subtitle: Text(muscleData.date),
-                          trailing: IconButton(
-                            icon: Icon(Icons.edit),
-                          )),
-                    ))
-                .toList();
-            return ListView(
-              children: listTiles,
-            );
-            //return Text(model.muscleData.toString());
-          },
-        ),
-      ),
-    );
+                        child: ListTile(
+                            leading: Image.network(muscleData.imageURL),
+                            title: Text(muscleData.weight.toString() + ' kg'),
+                            subtitle: Text(muscleData.date),
+                            trailing: IconButton(
+                              icon: Icon(Icons.edit),
+                            )),
+                      ))
+                  .toList();
+              return ListView(
+                children: listTiles,
+              );
+            },
+          ),
+          floatingActionButton: Consumer<ListModel>(
+            builder: (context, model, child) {
+              return FloatingActionButton.extended(
+                label: Text(model.sortName),
+                onPressed: () async {
+                  model.showBottomSheet(context);
+                },
+              );
+            },
+          ),
+        ));
   }
 }
