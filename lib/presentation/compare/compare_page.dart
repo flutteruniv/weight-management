e413854e5931cdc1ee2d6cefe554b25e69fb7928
wakanao <rockwakata15@bpe.agr.hokudai.ac.jp>
2020-11-19 13:46:41 +0900

    compareブランチを作成

--- lib/presentation/compare/compare_page.dart
+++ lib/presentation/compare/compare_page.dart
@@ -5,7 +5,7 @@ class ComparePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("compare"),
+      home: Text("compare作成"),
     );
   }
 }
