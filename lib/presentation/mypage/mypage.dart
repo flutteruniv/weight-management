6b61ca82d62c1bfa75454d09c899e62855524314
山本眞也 <>
2020-12-02 18:24:09 +0900

    新規登録用のボタンとログイン用のボタンをとりあえずマイページに設置

--- lib/presentation/mypage/mypage.dart
+++ lib/presentation/mypage/mypage.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:weight_management/presentation/login/login_page.dart';
+import 'package:weight_management/presentation/signup/signup_page.dart';
 
 class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: RaisedButton(
-        child: Text('ログアウト'),
-        onPressed: () {},
+      child: Column(
+        children: [
+          RaisedButton(
+            child: Text('新規登録'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SignUpPage()),
+              );
+            },
+          ),
+          RaisedButton(
+            child: Text('ログイン'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
+          ),
+          RaisedButton(
+            child: Text('ログアウト'),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
