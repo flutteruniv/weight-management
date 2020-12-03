2a40961ee5ed7608493f1e9849757078efc0b775
山本眞也 <>
2020-12-03 11:56:58 +0900

    仮でログアウトボタンを押すと認証用ページに遷移するようにした。

--- lib/presentation/mypage/mypage.dart
+++ lib/presentation/mypage/mypage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weight_management/presentation/login/login_page.dart';
-import 'package:weight_management/presentation/signup/signup_page.dart';
+import 'package:weight_management/presentation/authentication/authentication_page.dart';
 
 class MyPage extends StatelessWidget {
   @override
@@ -9,26 +8,13 @@ class MyPage extends StatelessWidget {
       child: Column(
         children: [
           RaisedButton(
-            child: Text('新規登録'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SignUpPage()),
-              );
-            },
-          ),
-          RaisedButton(
-            child: Text('ログイン'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LoginPage()),
-              );
-            },
-          ),
-          RaisedButton(
             child: Text('ログアウト'),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AuthenticationPage()),
+              );
+            },
           ),
         ],
       ),
