1844ff33bd1c96adaf321aeb2533b5ebd2233ca1
山本眞也 <>
2020-12-05 20:30:45 +0900

    マイページで入力した内容をcloud_firestoreのusersの配下に保存できるよう変更 ログイン中のユーザーが入力した内容がマイページに表示できるようになった

--- lib/presentation/mypage/mypage_model.dart
+++ lib/presentation/mypage/mypage_model.dart
@@ -1,23 +1,41 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:weight_management/domain/ideal_muscle_data.dart';
+import 'package:weight_management/domain/user.dart';
 
 class MyPageModel extends ChangeNotifier {
   double idealWeight;
   double idealFat;
   File idealImageFile;
   String idealImagePath;
+  List<Users> userData = [];
+  String userDocID;
   List<IdealMuscleData> idealMuscleList = [];
   IdealMuscleData idealMuscle;
   TextEditingController idealWeightTextController, idealFatTextController;
   bool hasIdealMuscle = false;
 
   Future fetchData() async {
-    final docs =
-        await FirebaseFirestore.instance.collection('idealMuscleData').get();
+    final docss = await FirebaseFirestore.instance.collection('users').get();
+    final userData = docss.docs.map((doc) => Users(doc)).toList();
+    this.userData = userData;
+    for (int i = 0; i < userData.length; i++) {
+      if (userData[i].userID == FirebaseAuth.instance.currentUser.uid) {
+        userDocID = userData[i].documentID;
+        break;
+      }
+    }
+
+    final docs = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userDocID)
+        .collection('idealMuscleData')
+        .orderBy('date', descending: true)
+        .get();
     final muscleData = docs.docs.map((doc) => IdealMuscleData(doc)).toList();
     this.idealMuscleList = muscleData;
     idealMuscle = idealMuscleList[0];
@@ -48,13 +66,19 @@ class MyPageModel extends ChangeNotifier {
 
   Future addDataToFirebase() async {
     //firebaseに値を追加
+    final firebaseUser = FirebaseAuth.instance.currentUser;
+
     if (idealWeight == null) {
       throw ('体重は入力するんだ！');
     }
     if (idealImageFile != null && idealFat != null) {
       //写真と体脂肪率があるとき
       final imageURL = await _uploadImage();
-      await FirebaseFirestore.instance.collection('idealMuscleData').add(
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userDocID)
+          .collection('idealMuscleData')
+          .add(
         {
           'weight': idealWeight,
           'bodyFatPercentage': idealFat,
@@ -64,7 +88,11 @@ class MyPageModel extends ChangeNotifier {
       );
     } else if (idealImageFile == null && idealFat != null) {
       //写真なし＆体脂肪率あり
-      await FirebaseFirestore.instance.collection('idealMuscleData').add(
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userDocID)
+          .collection('idealMuscleData')
+          .add(
         {
           'weight': idealWeight,
           'bodyFatPercentage': idealFat,
@@ -73,7 +101,11 @@ class MyPageModel extends ChangeNotifier {
     } else if (idealImageFile != null && idealFat == null) {
       //写真アリ＆体脂肪率なし
       final imageURL = await _uploadImage();
-      await FirebaseFirestore.instance.collection('idealMuscleData').add(
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userDocID)
+          .collection('idealMuscleData')
+          .add(
         {
           'weight': idealWeight,
           'imageURL': imageURL,
@@ -82,7 +114,11 @@ class MyPageModel extends ChangeNotifier {
       );
     } else if (idealImageFile == null && idealFat == null) {
       //写真なし＆体脂肪率なし
-      await FirebaseFirestore.instance.collection('idealMuscleData').add(
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userDocID)
+          .collection('idealMuscleData')
+          .add(
         {
           'weight': idealWeight,
         },
@@ -98,6 +134,8 @@ class MyPageModel extends ChangeNotifier {
       //写真と体脂肪率があるとき
       final imageURL = await _uploadImage();
       final document = FirebaseFirestore.instance
+          .collection('users')
+          .doc(userDocID)
           .collection('idealMuscleData')
           .doc(idealMuscle.documentID);
       await document.update({
@@ -108,6 +146,8 @@ class MyPageModel extends ChangeNotifier {
       });
     } else if (idealImageFile == null && idealFat != null) {
       final document = FirebaseFirestore.instance
+          .collection('users')
+          .doc(userDocID)
           .collection('idealMuscleData')
           .doc(idealMuscle.documentID);
       await document.update({
@@ -117,6 +157,8 @@ class MyPageModel extends ChangeNotifier {
     } else if (idealImageFile != null && idealFat == null) {
       final imageURL = await _uploadImage();
       final document = FirebaseFirestore.instance
+          .collection('users')
+          .doc(userDocID)
           .collection('idealMuscleData')
           .doc(idealMuscle.documentID);
       await document.update({
@@ -126,6 +168,8 @@ class MyPageModel extends ChangeNotifier {
       });
     } else if (idealImageFile == null && idealFat == null) {
       final document = FirebaseFirestore.instance
+          .collection('users')
+          .doc(userDocID)
           .collection('idealMuscleData')
           .doc(idealMuscle.documentID);
       await document.update({
