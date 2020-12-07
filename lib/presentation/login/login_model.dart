9dac03652626454b28b18dca79e291a76342fe45
wakanao <rockwakata15@bpe.agr.hokudai.ac.jp>
2020-12-08 00:57:40 +0900

    パスワードリセットメールを送信できるようになった

--- lib/presentation/login/login_model.dart
+++ lib/presentation/login/login_model.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
 class LoginModel extends ChangeNotifier {
@@ -6,6 +7,54 @@ class LoginModel extends ChangeNotifier {
   String password = '';
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  String email;
+
+  Future<String> sendPasswordResetEmail(
+      String email, BuildContext context) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+      Navigator.pop(context);
+      Flushbar(
+        message: "パスワードリセットメールを送信しました",
+        backgroundColor: Colors.blue,
+        margin: EdgeInsets.all(8),
+        borderRadius: 8,
+        duration: Duration(seconds: 3),
+      )..show(context);
+    } catch (error) {
+      switch (error.code) {
+        case 'invalid-email':
+          Flushbar(
+            message: "無効なメールアドレスです",
+            backgroundColor: Colors.red,
+            margin: EdgeInsets.all(8),
+            borderRadius: 8,
+            duration: Duration(seconds: 3),
+          )..show(context);
+          break;
+        case 'user-not-found':
+          Flushbar(
+            message: "メールアドレスが登録されていません",
+            backgroundColor: Colors.red,
+            margin: EdgeInsets.all(8),
+            borderRadius: 8,
+            duration: Duration(seconds: 3),
+          )..show(context);
+          break;
+        default:
+          Flushbar(
+            message: "メール送信に失敗しました",
+            backgroundColor: Colors.red,
+            margin: EdgeInsets.all(8),
+            borderRadius: 8,
+            duration: Duration(seconds: 3),
+          )..show(context);
+          break;
+      }
+    }
+    notifyListeners();
+  }
 
   Future login() async {
     if (mail.isEmpty) {
--- lib/presentation/login/login_page.dart
+++ lib/presentation/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weight_management/presentation/login/login_model.dart';
@@ -76,6 +77,92 @@ class LoginPage extends StatelessWidget {
                   ),
                   SizedBox(
                     height: 30,
+                  ),
+                  InkWell(
+                      child: Text(
+                        "パスワードを忘れたマッスルへ",
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onTap: () async {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text(
+                                'ログインできない場合',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 25, fontWeight: FontWeight.bold),
+                              ),
+                              content: SingleChildScrollView(
+                                child: Container(
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        'パスワードリセットメールを送ります\nメールアドレスを入力してください。',
+                                        style: TextStyle(fontSize: 13),
+                                      ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      TextField(
+                                        onChanged: (text) {
+                                          model.email = text;
+                                        },
+                                        decoration: InputDecoration(
+                                          hintText: 'メールアドレスを入力してください',
+                                          labelText: "メールアドレス",
+                                          enabledBorder: new OutlineInputBorder(
+                                            borderRadius:
+                                                new BorderRadius.circular(4.0),
+                                            borderSide: BorderSide(
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                          focusedBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                new BorderRadius.circular(4.0),
+                                            borderSide: BorderSide(
+                                              color: Colors.blue,
+                                              width: 3.0,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              actions: <Widget>[
+                                FlatButton(
+                                  child: Text(
+                                    'キャンセル',
+                                    style: TextStyle(color: Colors.blue),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                FlatButton(
+                                  child: Text(
+                                    '送信',
+                                    style: TextStyle(color: Colors.red),
+                                  ),
+                                  onPressed: () async {
+                                    await model.sendPasswordResetEmail(
+                                        model.email, context);
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }),
+                  SizedBox(
+                    height: 10,
                   ),
                   ButtonTheme(
                     minWidth: double.infinity,
